27a82aebfdbb4cca515973f4416c5efe1ffa0061
RayOfDoom <spectrumlazergaming@gmail.com>
2022-07-28 09:30:16 +0800

    trying to fix cookie bugs

--- website/routers/auth.py
+++ website/routers/auth.py
@@ -222,8 +222,7 @@ async def login_user_for_access_token(
         scopes=scopes
     )
     access_token = jwt.encode(token_info.dict(), JWT_SECRET)
-    response.set_cookie(key="access_token", value=f"Bearer {access_token}", httponly=True, path='/', samesite="None",
-                        secure=True)
+    response.set_cookie(key="access_token", value=f"Bearer {access_token}", httponly=True, path='/', samesite="None")
     return {'access_token': access_token, 'token_type': 'bearer'}
 
 
@@ -243,7 +242,7 @@ async def logout_user(
         response: Response,
         current_user: schemas.User = Security(get_current_user_required, scopes=['me'])
 ):
-    response.delete_cookie("access_token", httponly=True, path='/', samesite="None", secure=True)
+    response.delete_cookie("access_token", httponly=True, path='/', samesite="None")
     return
 
 
